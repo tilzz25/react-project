2d20bce0cc416cf97033dd98e0aa64e3cd570310
tilzz25 <tilakp1151@gmail.com>
2025-03-12 17:13:54 +0300

    hehe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FormComponent from './component/formComponent';
+import LogInComponent from './component/LogInComponent';
 
 function App() {
   return (
     <div >
       <FormComponent/>
+      <LogInComponent/>
     </div>
   );
 }
