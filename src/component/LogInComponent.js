94bf24207330412bf472c5bce99c227b72ba1a22
tilzz25 <tilakp1151@gmail.com>
2025-03-12 17:15:00 +0300

    commit

--- src/component/LogInComponent.js
+++ src/component/LogInComponent.js
@@ -5,7 +5,7 @@ const LogInComponent = () => {
            <input id="email" name="email" type="text" placeholder="Email" required />
            <label for="Password"></label>
             <input id="password" name="Password" type="password" placeholder="Password" required />
-            <button id="submitButton">SUBMIT</button>
+            <button id="submitButton">LogIn</button>
         </div>
     )
     }
