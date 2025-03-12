60d07f03c1f068963abc3ab88c1c60609a6a4aa7
tilzz25 <tilakp1151@gmail.com>
2025-03-12 17:13:10 +0300

    formcomponent.js

---
+++ src/component/LogInComponent.js
@@ -0,0 +1,13 @@
+const LogInComponent = () => {
+    return(
+        <div>
+           <label for="Email"></label>
+           <input id="email" name="email" type="text" placeholder="Email" required />
+           <label for="Password"></label>
+            <input id="password" name="Password" type="password" placeholder="Password" required />
+            <button id="submitButton">SUBMIT</button>
+        </div>
+    )
+    }
+    
+    export default LogInComponent
