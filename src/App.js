172e86ce88fb203caff6d706499aa89e3ce5549f
tilzz25 <tilakp1151@gmail.com>
2025-03-13 16:25:01 +0300

    added css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import FormComponent from './component/formComponent';
-import LogInComponent from './component/LogInComponent';
+import FormComponent from './components/formComponent';
+import LogInComponent from './components/LogInComponent';
+import { useState } from 'react';
+import Navbar from './components/navBar';
+import FooterComp from './components/footer';
 
 function App() {
-  const handleForm = (e) => {
-    const BtnName = e.target.id
-    console.log(BtnName)
+  const [formState, setFormState] = useState("register"); 
+   const handleForm = (e) => {
+    const btnName = e.target.value
+    setFormState(btnName)
+    console.log(formState)
   }
   return (
-    <div >
-      <button id="loginbutton" onClick={handleForm}>LOGIN</button>
-      <button id="registerButton" onClick={handleForm}>REGISTER</button>
-      <FormComponent/>
-      <LogInComponent/>
+    <div>
+      <Navbar />
+     <div className='tab-container' style={{ marginBottom: '20px'}}>
+     <button className="btn"value="register"  onClick={handleForm}>REGISTER</button>
+     <button className="btn" onClick={handleForm} >LOGIN</button>
+     </div >
+      { formState === "register" ? <FormComponent/> : <LogInComponent/> }
+      <FooterComp style={{ marginTop: '20px' }}/>          
+             
     </div>
   );
 }
