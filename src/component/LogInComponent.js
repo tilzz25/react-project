e68f0999332345a787574f463b9cd235a4db3848
tilzz25 <tilakp1151@gmail.com>
2025-03-13 16:25:27 +0300

    commited

--- src/component/LogInComponent.js
+++ src/component/LogInComponent.js
@@ -1,14 +1,21 @@
+import "./LogInComponent.css"
+
+
 const LogInComponent = () => {
-    return(
-        <div>
-            <h1> LOGIN</h1>
-           <label for="Email"></label>
-           <input id="email" name="email" type="text" placeholder="Email" required />
-           <label for="Password"></label>
-            <input id="password" name="Password" type="password" placeholder="Password" required />
-            <button id="submitButton">LogIn</button>
+    return (
+        <div className="login-container">
+            <h1 className="title">LOGIN</h1>
+            
+            <label htmlFor="email1">Email</label>
+            <input id="email1" name="email" type="email" placeholder="Enter Email" required />
+            
+            <label htmlFor="password1">Password</label>
+            <input id="password1" name="password" type="password" placeholder="Enter Password" required />
+            
+            <button className="btn2" id="submitButton">LOGIN</button>
+            
+            <p className="forgot-password">Forgot password?</p>
         </div>
-    )
-    }
-    
-    export default LogInComponent
+    );
+}
+export default LogInComponent;
